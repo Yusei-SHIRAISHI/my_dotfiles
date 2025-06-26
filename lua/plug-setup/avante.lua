3f4b86d2445e3fca2665ee6843dddac3f00cca9e
Yusei-SHIRAISHI <yusei.shiraishi29@gmail.com>
2025-06-26 17:36:34 +0900

    updated

--- lua/plug-setup/avante.lua
+++ lua/plug-setup/avante.lua
@@ -1,17 +1,11 @@
 return function()
   require("avante").setup({
+    mode = 'agentic',
     provider = "copilot",
     providers = {
       copilot = {
-        endpoint = "https://api.githubcopilot.com",
-        model = "claude-sonnet-4",
-        proxy = nil, -- [protocol://]host[:port] Use this proxy
-        allow_insecure = false, -- Allow insecure server connections
-        timeout = 30000, -- Timeout in milliseconds
-        extra_request_body = {
-          temperature = 0,
-          max_tokens = 20480,
-        },
+        model = "gpt-4o",
+        --model = "claude-sonnet-4",
       },
     },
     auto_suggestions_provider = "copilot",
